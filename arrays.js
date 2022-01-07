47f657f3375faee3347ec33fb2100d45beb5187e
Braden Dickerson <purpledragon6445@gmail.com>
2022-01-07 09:32:00 -0700

    Working on arrays test

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+var colorCopy = faveColors.slice(0,1,2)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
